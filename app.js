533a39c355cd2bb2d1aa04fe136e5f3a95d3896c
ksu <ksundelife@yandex.ru>
2021-09-26 12:18:48 +0300

    modified js

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const slides =document.querySelectorAll('.slide');
+function sliderPlugin(activeSlide = 0) {
+	const slides =document.querySelectorAll('.slide');
 
-// for (const slide of slides) {
-// 	slide.addEventListener('click', () => {
-// 		clearActiveClasses();
-// 		slide.classList.add('active');
-// 	})
-// }
-slides.forEach((element) => {
-	element.addEventListener('click', () => {
-		clearActiveClasses();
-		element.classList.add('active');
-	})
-});
+	slides[activeSlide].classList.add('active');
 
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active');
+	slides.forEach((element) => {
+		element.addEventListener('click', () => {
+			clearActiveClasses();
+			element.classList.add('active');
+		})
 	});
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active');
+		});
+	}
 }
+sliderPlugin(3);
